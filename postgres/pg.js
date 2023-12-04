5e7612288d818e57b29d54123473200d93a4aa65
Chris Rybicki <chrisr@monada.co>
2023-12-03 21:21:10 -0800

    feat(postgres): sim support (#5)

--- postgres/pg.js
+++ postgres/pg.js
@@ -1,15 +1,22 @@
 const pg = require("pg");
 
-exports._query = async function(query, credentials) {
-  const { host, user, password, dbname } = credentials;
-  const client = new pg.Client({
-    host,
-    user,
-    password,
-    database: dbname,
-    ssl: true,
-  });
-  await client.connect();
+exports._query = async function(query, opts) {
+  const client = await connectWithRetry(opts);
   const res = await client.query(query);
+  await client.end();
   return res.rows;
+}
+
+// workaround for readiness probe
+async function connectWithRetry(opts, maxRetries = 5, interval = 1000) {
+  for (let i = 0; i < maxRetries; i++) {
+    try {
+      const client = new pg.Client(opts);
+      await client.connect();
+      return client;
+    } catch (error) {
+      if (i === maxRetries - 1) throw error;
+      await new Promise(resolve => setTimeout(resolve, interval));
+    }
+  }
 }
