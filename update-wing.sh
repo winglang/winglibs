91daff58f341e00d7d6b7f1eb73b588b3a792495
Pol Amorós <info@polamoros.com>
2024-01-18 09:38:16 +0100

    chore: update wing version (#61)
    - Updates wing to the latest version in each winglib.
    - Adds a script to update wing (would be nice to do it in a cron job).

---
+++ update-wing.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+for dir in */ ; do
+    if [ -f "$dir/package.json" ]; then
+        cd "$dir"        
+        if grep -q "\"winglang\"" package.json; then
+            echo "Updating winglang in '$dir'..."
+            npm update winglang
+        else
+            echo "winglang not found in '$dir'.\n"
+        fi        
+        cd -
+    fi
+done
