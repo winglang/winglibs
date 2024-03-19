8aac431525d03bbc6608382a237d9a096a150e8c
Elad Ben-Israel <eladb@monada.co>
2024-03-19 17:27:08 +0200

    chore(openai): a few refactors (#134)
    * Add openai lib
    
    * change git clone in the readme to the public url
    
    * add description to package.json
    
    * update mklib.sh to include a description in package.json
    
    * fix PR comment about peerDependencies
    
    * tweaks
    
    * remove author from template
    
    * Update openai.test.w
    
    * fix test
    
    * another fix to test
    
    * more cleanups
    
    * Update package.json
    
    * Update openai.test.w
    
    * Rename openai/example.main.w to openai/examples/example.main.w
    
    * fix bring statement in examples
    
    * remove unused files
    
    ---------
    
    Co-authored-by: Shai Ber <shaib@monada.co>

--- openai/openai.js
+++ openai/openai.js
@@ -9,30 +9,9 @@ exports.createNewInflightClient = (apiKey, org) => {
 
   let client = new openai.OpenAI(config);
 
-  // TODO: this is a hack for now, we should model the openai api in the api.w file with more fidelity
-  // and then we can just return the client itself, like we do in redis
   return {
-    createCompletion: async (prompt, params = {}) => {
-      if (!prompt) {
-        throw new Error("Prompt is required");
-      };
-    
-      if (typeof prompt !== "string") {
-        throw new Error("Prompt must be a string");
-      }
-    
-      if (!params.model) {
-        params.model = "gpt-3.5-turbo";
-      };
-    
-      if (!params.max_tokens) {
-        params.max_tokens = 2048;
-      }
-    
-      params.messages = [ { role: 'user', content: prompt } ];
-      
-      const response = await client.chat.completions.create(params);
-      return response.choices[0]?.message?.content;
+    createCompletion: async params => {
+      return await client.chat.completions.create(params);
     }
   };
 };
