46476948c249316c4395b0b99446fce8ab952448
Cristian Pallarés <cristianp@wing.cloud>
2024-03-19 14:51:29 +0100

    feat: `bring dynamodb` (#98)
    - Interface similar to the original DynamoDB SDK
    - Includes streams support
    - Uses one container for all tables (sim)
    
    Closes winglang/wing#4949

---
+++ dynamodb/dynamodb.mjs
@@ -0,0 +1,140 @@
+export { default as getPort } from "get-port";
+
+import * as dynamodb from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+export { unmarshall };
+
+export const createClient = (options) => {
+  return new dynamodb.DynamoDB(options);
+};
+
+export const createDocumentClient = (options) => {
+  const client = createClient(options);
+  return DynamoDBDocument.from(client, {
+    marshallOptions: {
+      removeUndefinedValues: true,
+      convertEmptyValues: true,
+    },
+    unmarshallOptions: {
+      wrapNumbers: true,
+    },
+  });
+};
+
+import * as streams from "@aws-sdk/client-dynamodb-streams";
+
+/**
+ * @param {import("@aws-sdk/client-dynamodb-streams").DynamoDBStreams} client
+ * @param {string} StreamArn
+ * @param {(record: any) => void|Promise<void>} handler
+ */
+const processStreamRecords = async (client, StreamArn, handler, options) => {
+  const { StreamDescription } = await client.describeStream({ StreamArn });
+
+  for (const { ShardId } of StreamDescription.Shards) {
+    const shardIteratorData = await client.getShardIterator({
+      StreamArn,
+      ShardId,
+      ShardIteratorType: options?.startingPosition ?? "LATEST",
+    });
+
+    let shardIterator = shardIteratorData.ShardIterator;
+    while (shardIterator) {
+      const recordsData = await client.getRecords({
+        ShardIterator: shardIterator,
+        Limit: options?.batchSize,
+      });
+
+      for (const record of recordsData.Records) {
+        try {
+          await handler({
+            eventId: record.eventID,
+            eventName: record.eventName,
+            dynamodb: {
+              ApproximateCreationDateTime:
+                record.dynamodb.ApproximateCreationDateTime,
+              Keys: record.dynamodb.Keys
+                ? unmarshall(record.dynamodb.Keys, {
+                    wrapNumbers: true,
+                  })
+                : undefined,
+              NewImage: record.dynamodb.NewImage
+                ? unmarshall(record.dynamodb.NewImage, {
+                    wrapNumbers: true,
+                  })
+                : undefined,
+              OldImage: record.dynamodb.OldImage
+                ? unmarshall(record.dynamodb.OldImage, {
+                    wrapNumbers: true,
+                  })
+                : undefined,
+              SequenceNumber: record.dynamodb.SequenceNumber,
+              SizeBytes: record.dynamodb.SizeBytes,
+              StreamViewType: record.dynamodb.StreamViewType,
+            },
+          });
+        } catch (error) {
+          console.error("Error processing stream record:", error, record);
+        }
+      }
+
+      shardIterator = recordsData.NextShardIterator;
+
+      // AWS DynamoDB fetches records 4 times a second.
+      await new Promise((resolve) => setTimeout(resolve, 250));
+    }
+  }
+};
+
+const processRecords = async (endpoint, tableName, handler, options) => {
+  while (true) {
+    const client = new streams.DynamoDBStreams({
+      region: "local",
+      credentials: {
+        accessKeyId: "local",
+        secretAccessKey: "local",
+      },
+      endpoint,
+    });
+
+    try {
+      const { Streams } = await client.listStreams({ TableName: tableName });
+      await Promise.all(
+        Streams.map(({ StreamArn }) =>
+          processStreamRecords(client, StreamArn, handler, options)
+        )
+      );
+    } catch (error) {
+      if (error.message.includes("ECONNREFUSED")) {
+        // Stop processing records when the host is down.
+        throw error;
+      } else if (error.name === "TrimmedDataAccessException") {
+        // Ignore. This error seems to be a bug in DynamoDB Local.
+        // The desired behavior is to retry processing the records.
+      } else if (error.name === "ExpiredIteratorException") {
+        // Ignore. This error happens after the computer wakes up from sleep.
+      } else {
+        throw error;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 250));
+    }
+  }
+};
+
+export const processRecordsAsync = async (
+  endpoint,
+  tableName,
+  handler,
+  options
+) => {
+  processRecords(endpoint, tableName, handler, options).catch((error) => {
+    if (error.message.includes("ECONNREFUSED")) {
+      // Ignore. This error happens when reloading the console.
+      // We can safely end the execution here.
+    } else {
+      console.error(error);
+    }
+  });
+};
---
+++ dynamodb/tests/compatibility.gsi.test.w
@@ -0,0 +1,96 @@
+bring "../dynamodb.w" as dynamodb;
+
+let table = new dynamodb.Table(
+    name: "blog",
+    attributes: [
+        {name: "type", type: "S"},
+        {name: "id", type: "S"},
+        {name: "createdAt", type: "N"},
+    ],
+    hashKey: "type",
+    rangeKey: "id",
+    globalSecondaryIndex: [{
+        name: "CreatedAtIndex",
+        hashKey: "type",
+        rangeKey: "createdAt",
+        projectionType: "ALL"
+    }],
+);
+
+// Only Hash Key
+let table2 = new dynamodb.Table(
+    name: "blog2",
+    attributes: [
+        {name: "type", type: "S"},
+        {name: "id", type: "S"},
+    ],
+    hashKey: "type",
+    rangeKey: "id",
+    globalSecondaryIndex: [{
+        name: "TypeIndex",
+        hashKey: "type",
+        projectionType: "ALL"
+    }],
+) as "blog2";
+
+test "GlobalSecondaryIndex" {
+    let idCreated = [
+        {id: "zuegksw", createdAt: 1},
+        {id: "dirnfhw", createdAt: 3},
+        {id: "pdkeruf", createdAt: 5},
+        {id: "azjekfw", createdAt: 7},
+    ];
+
+    for i in idCreated {
+        let id = i.get("id").asStr();
+        let createAt = i.get("createdAt").asNum();
+
+        table.put({
+            Item: {
+                "type": "post",
+                "id": id,
+                "title": "Title #{i}",
+                "createdAt": createAt,
+            },
+        });
+    }
+
+    let items = table.query({
+        KeyConditionExpression: "#type = :type",
+        ExpressionAttributeNames: {
+            "#type": "type",
+        },
+        ExpressionAttributeValues: {
+            ":type": "post",
+        },
+        ScanIndexForward: false,
+    });
+
+    // log("{Json.stringify(items.items)}");
+
+    // returns all items order by id desc
+    let ids = ["zuegksw", "pdkeruf", "dirnfhw", "azjekfw"];
+    for i in 0..ids.length {
+        assert(items.Items.at(i).get("id") == ids.at(i));
+    }
+
+    let itemsCreatedAtIndex = table.query(
+        IndexName: "CreatedAtIndex",
+        KeyConditionExpression: "#type = :type",
+        ExpressionAttributeNames: {
+            "#type": "type",
+        },
+        ExpressionAttributeValues: {
+            ":type": "post",
+        },
+        ScanIndexForward: false,
+    );
+
+    // log("{Json.stringify(itemsCreatedAtIndex.items)}");
+
+    // returns all items order by createdAt desc
+    let idsOrderByCreatedAt = ["azjekfw", "pdkeruf", "dirnfhw", "zuegksw"];
+    for i in 0..idsOrderByCreatedAt.length {
+        assert(itemsCreatedAtIndex.Items.at(i).get("id") == idsOrderByCreatedAt.at(i));
+    }
+}
