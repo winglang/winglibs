0d56ea1b1a9d6f38a8f7b98048da69b0216de96c
Chris Rybicki <chrisr@wing.cloud>
2024-08-30 14:52:56 -0400

    try twice, once with file and once without

--- k8s/test.sh
+++ k8s/test.sh
@@ -12,3 +12,18 @@ if wait $pid 2>/dev/null; then
 else
     echo "your_command interrupted"
 fi
+
+echo "Done"
+echo "Removing lib/blah.txt"
+
+rm lib/blah.txt
+
+( DEBUG="*" wing gen-docs ) & pid=$!
+( sleep 2 && kill -HUP $pid ) 2>/dev/null & watcher=$!
+if wait $pid 2>/dev/null; then
+    echo "your_command finished"
+    pkill -HUP -P $watcher
+    wait $watcher
+else
+    echo "your_command interrupted"
+fi
