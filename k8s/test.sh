89ed2ed607fc453c4c8ce86d51706fee9455e30f
Chris Rybicki <chrisr@wing.cloud>
2024-08-30 18:02:35 -0400

    remove some testing code

--- k8s/test.sh
+++ k8s/test.sh
@@ -1,29 +1,2 @@
 #!/bin/sh
-echo "Compiling..."
-wing compile .
-echo "Generating docs..."
-
-( DEBUG="*" wing gen-docs ) & pid=$!
-( sleep 2 && kill -HUP $pid ) 2>/dev/null & watcher=$!
-if wait $pid 2>/dev/null; then
-    echo "your_command finished"
-    pkill -HUP -P $watcher
-    wait $watcher
-else
-    echo "your_command interrupted"
-fi
-
 echo "Done"
-echo "Removing lib/"
-
-rm -rf lib
-
-( DEBUG="*" wing gen-docs ) & pid=$!
-( sleep 2 && kill -HUP $pid ) 2>/dev/null & watcher=$!
-if wait $pid 2>/dev/null; then
-    echo "your_command finished"
-    pkill -HUP -P $watcher
-    wait $watcher
-else
-    echo "your_command interrupted"
-fi
