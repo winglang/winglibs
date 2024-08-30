661c0465bcb1600654c8e57a66685fedcca23e20
Chris Rybicki <chrisr@wing.cloud>
2024-08-30 14:45:32 -0400

    try quotes

--- k8s/test.sh
+++ k8s/test.sh
@@ -2,7 +2,7 @@
 echo "Compiling..."
 wing compile .
 echo "Generating docs..."
-timeout 5s DEBUG="*" wing gen-docs
+timeout 5s 'DEBUG="*" wing gen-docs'
 
 if [ $? -eq 124 ]; then
     echo "Command timed out after 5 seconds"
