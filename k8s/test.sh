cf14490a5dbc389aae4f898597746dde3779ed42
Chris Rybicki <chrisr@wing.cloud>
2024-08-30 13:50:20 -0400

    remove tests

--- k8s/test.sh
+++ k8s/test.sh
@@ -1,27 +1,2 @@
 #!/bin/sh
-t() {
-  echo "$1 ================"
-  out=$(wing compile -t ./lib/index.js $1)
-
-  cat $out/*.yaml > $1.actual.snap
-  cat $1.actual.snap
-
-  if [ -f "$1.snap" ]; then
-    diff $1.snap $1.actual.snap
-    if [ $? -eq 0 ]; then
-      echo "$1: passed"
-    else
-      echo "$1: failed, updating snapshot"
-      cp $1.actual.snap $1.snap
-      rm $1.actual.snap
-      exit 1
-    fi
-  else
-    cp $1.actual.snap $1.snap
-  fi
-
-  rm $1.actual.snap
-}
-
-t examples/nodejs.main.w
-t examples/ubuntu.main.w
+echo Done
