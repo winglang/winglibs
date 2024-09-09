0330544e102a792084ebd939ebffe82d8c935389
Chris Rybicki <chrisr@wing.cloud>
2024-09-09 19:09:04 -0400

    chore(tsoa): update .d.ts files (#318)

--- test-all.sh
+++ test-all.sh
@@ -3,19 +3,34 @@ set -euo pipefail
 
 WING=${WING:-wing}
 
-for i in $(ls -F); do
-  if [ -f "$i/package.json" ]; then
-    echo "-----------------------------------------------------------------------------"
-    echo $i
-    echo "-----------------------------------------------------------------------------"
-    (
-      cd $i
-      npm i
-      if [ -x test.sh ]; then
-        ./test.sh
-      else
-        $WING test
-      fi
-    )
+# Read the skipped packages from the JSON file
+skipped_packages=$(jq -r '.["skipped-packages"][].name' SKIPPED_LIBRARIES.json)
+
+for package_file in */package.json; do
+  # Check if the package.json file exists
+  if [[ ! -f "$package_file" ]]; then
+      continue
   fi
+
+  package_name="${package_file%/package.json}"
+
+  echo "-----------------------------------------------------------------------------"
+  echo $package_name
+  echo "-----------------------------------------------------------------------------"
+
+  # Check if the package should be skipped
+  if echo "$skipped_packages" | grep -q "^$package_name$"; then
+    echo "Skipping $package_name"
+    continue
+  fi
+
+  (
+    cd $package_name
+    npm i
+    if [ -x test.sh ]; then
+      ./test.sh
+    else
+      $WING test
+    fi
+  )
 done
